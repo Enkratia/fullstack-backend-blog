6e85b8472000fbd239a2bbac80e363e6e73e1307
Enkratia <manofthetreasure@gmail.com>
2024-03-17 21:09:14 +0300

    m

--- src/_tasks/_tasks.service.ts
+++ src/_tasks/_tasks.service.ts
@@ -7,9 +7,9 @@ import { SubscribeService } from '../subscribe/subscribe.service';
 export class TasksService {
   constructor(private subscribeService: SubscribeService) {}
 
-  @Cron('0 */2 * * * *')
+  // @Cron('0 */2 * * * *')
   async sendMailToSubscribers() {
-    await this.subscribeService.sendMailToSubscribers();
+    // await this.subscribeService.sendMailToSubscribers();
     console.log('cron');
   }
 }
